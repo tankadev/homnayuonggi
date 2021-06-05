43872d7fdf8ed344386d8665d743bac0ee5f5783
tankadev <nguyentankadev@gmail.com>
2021-06-05 23:58:24 +0700

    update UI order items

--- src/app/components/order-content/order-content.component.ts
+++ src/app/components/order-content/order-content.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { map } from 'rxjs/operators';
 
@@ -14,9 +14,8 @@ import { OrderService } from 'src/app/services/order.service';
 export class OrderContentComponent implements OnInit {
 
   listOrders: OrderRO[] = [];
-  deadline = 1000 * 60 * 60 * 24 * 2 + 1000 * 30;
+  deadline = Date.now() + 1000 * 60 * 60 * 0.5;
   myApiRoute = 'https://www.now.vn/can-tho/nuoc-mia-my-tho-mt68';
-  links = ['https://www.now.vn/can-tho/nuoc-mia-my-tho-mt68'];
 
   constructor(
     private orderService: OrderService,
@@ -51,12 +50,8 @@ export class OrderContentComponent implements OnInit {
       console.log('delete all successfully!');
     });
   }
-  
-  public apiCallbackFn = (route: string) => {
-    return this.http.get('https://www.now.vn/can-tho/nuoc-mia-my-tho-mt68');
-  }
 
-  public previewClick(link: string): void {
-    alert('Link:\n' + link + '\nwas clicked!');
+  orderFinish(event) {
+    console.log(event);
   }
 }
