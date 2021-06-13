f9123c049a4efa9d9dd1c41bb97336d3f167b91b
tankadev <nguyentankadev@gmail.com>
2021-06-14 00:18:29 +0700

    Update UI dish

--- src/app/ui/place-order/list-dish/list-dish.component.ts
+++ src/app/ui/place-order/list-dish/list-dish.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { DeliveryRO } from 'src/app/ro/delivery.ro';
 
 @Component({
   selector: 'list-dish',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-dish.component.scss']
 })
 export class ListDishComponent implements OnInit {
+
+  @Input() deliveryInfo: DeliveryRO;
 
   constructor() { }
 
