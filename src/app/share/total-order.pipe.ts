bfd994036cea0a65112739b8b7caf7f46e554ba2
hoaivan <van.nguyen@deltabrains.tech>
2022-07-12 14:11:54 +0700

    hotfix total order

--- src/app/share/total-order.pipe.ts
+++ src/app/share/total-order.pipe.ts
@@ -2,6 +2,7 @@ import { DecimalPipe } from '@angular/common';
 import { Pipe, PipeTransform } from '@angular/core';
 
 import { OrderRO } from '../ro/order.ro';
+import { RoomRO } from '../ro/room.ro';
 import { LocalStorageService } from '../services/localstorage.service';
 import { DishTotalQuantityPipe } from './dish-total-quantity.pipe';
 
@@ -17,8 +18,9 @@ export class TotalOrderPipe implements PipeTransform {
   ) {}
 
   transform(currency: string, listOrders?: OrderRO[]): string | number {
+    const room: RoomRO = this.storage.getSelectedRoom();
     let totalPrice = 0;
-    const orders = listOrders ? listOrders : this.storage.getOrdersList();
+    const orders = listOrders ? listOrders : this.storage.getOrdersList().filter(i => i.roomKey === room.key);;
     orders.forEach(order => {
       const price = order.dish.discountPrice ? order.dish.discountPrice.value : order.dish.price.value;
       totalPrice += (Number(price) * Number(this.totalQuantityPipe.transform(order.userNotes)));
